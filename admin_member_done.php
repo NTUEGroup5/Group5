59187bc61195c52a1a1f3fb5663bf21347e4dccd
chrisnien <chrisnien831113@gmail.com>
2014-01-03 11:14:19 +0800

    Update admin_member_done.php

--- admin_member_done.php
+++ admin_member_done.php
@@ -12,23 +12,23 @@
 	$button = $_POST['button'];
 ?>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>國立臺北教育大學_學習網</title>
+<title>毛線編織網</title>
 </head>
 
 <body>
 <div id="HEADER">
-	<h2>電腦科學學習網</h2>
+	<h2><FONT FACE="微軟正黑體">毛線編織家</FONT></h2>
 </div>
 <div id="MAIN_NAV">
 	<ul>
-		<li><a href="reading.php">開始閱讀</a></li>  
-		<li><a href="upload.php">檔案上傳</a></li>
-		<li><a href="message.php">留言板</a></li>
-		<li><a href="record.php">歷史紀錄</a></li>
-		<li><a href="login.php">會員資料修改</li>
-		<li><a href="group.php">管理團隊</a></li>
-		<li><b>管理者專區</b></li>
-		<li><a href="index.php" style="color:#FF99FF">回首頁</a></li>
+		<a href="reading.php"><FONT FACE="微軟正黑體">針織大學</FONT></a>
+		<a href="upload.php"><FONT FACE="微軟正黑體">毛絨絨分享區</FONT></a>
+		<a href="message.php"><FONT FACE="微軟正黑體">打結了怎麼辦</FONT></a>
+		<a href="record.php"><FONT FACE="微軟正黑體">我的足跡</FONT></a>
+		<a href="login.php"><FONT FACE="微軟正黑體">My Secret 修改</FONT>
+		<a href="group.php"><FONT FACE="微軟正黑體">毛絨絨團隊</FONT></a>
+		<b><FONT FACE="微軟正黑體">毛怪的家</FONT></b>
+		<a href="index.php" style="color:#FF99FF"><FONT FACE="微軟正黑體">回首頁頁</FONT></a>
 	</ul>
 </div>
 <div id="CONTENT">
@@ -58,12 +58,6 @@
 		?>
 		</center>
 	</p>	
-</div>
-<div id="FOOTER">	
-	<p>
-		<br/><br/><br/><br/><br/><br/>
-		<h2><center><br/>Author by <i>Yi-Chan Kao</i> & <i>Kung-Si Cheng</i> </center></h2>
-	</p>
-</div>
+
 </body>
 </html>
