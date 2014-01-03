3e4e8882c924052d8305f303d3672d7df396741a
chrisnien <chrisnien831113@gmail.com>
2014-01-03 11:33:32 +0800

    Update admin_member_edit.php

--- admin_member_edit.php
+++ admin_member_edit.php
@@ -7,23 +7,26 @@
 	$button = $_POST['button1'];  
 ?>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>國立臺北教育大學_學習網</title>
+<title>毛線編織家</title>
 </head>
 
 <body>
+
+<body background="http://www.checkfun.com.tw/shop/Stores_APP/images/images_115/CAT%E6%8A%93%E6%AF%9B%E7%B7%9A%E5%9C%96.jpg">
+
 <div id="HEADER">
-	<h2>電腦科學學習網</h2>
+	<h2><FONT FACE="微軟正黑體">毛線編織家</h2>
 </div>
 <div id="MAIN_NAV">
 	<ul>
-		<li><a href="reading.php">開始閱讀</a></li>
-		<li><a href="upload.php">檔案上傳</a></li>
-		<li><a href="message.php">留言板</a></li>
-		<li><a href="record.php">歷史紀錄</a></li>
-		<li><a href="login.php">會員資料修改</li>
-		<li><a href="group.php">管理團隊</a></li>
-		<li><b>管理者專區</b></li>
-		<li><a href="index.php" style="color:#FF99FF">回首頁</a></li>
+		<li><a href="reading.php"><FONT FACE="微軟正黑體">針織大學</FONT></a></li>
+		<li><a href="upload.php"><FONT FACE="微軟正黑體">毛茸茸分享區</FONT></a></li>
+		<li><a href="message.php"><FONT FACE="微軟正黑體">打結了怎麼辦</FONT></a></li>
+		<li><a href="record.php"><FONT FACE="微軟正黑體">我的足跡</FONT></a></li>
+		<li><a href="login.php"><FONT FACE="微軟正黑體">毛茸茸團隊</FONT></li>
+		<li><a href="group.php"><FONT FACE="微軟正黑體">毛怪的家</FONT></a></li>
+		<li><b><FONT FACE="微軟正黑體">管理者專區</FONT></b></li>
+		<li><a href="index.php" style="color:#FF99FF"><FONT FACE="微軟正黑體">回首頁</FONT></a></li>
 	</ul>
 </div>
 <div id="CONTENT">
@@ -91,12 +94,6 @@
 		?>
 		</center>
 	</p>	
-</div>
-<div id="FOOTER">	
-	<p>
-		<br/><br/><br/><br/><br/><br/>
-		<h2><center><br/>Author by <i>Yi-Chan Kao</i> & <i>Kung-Si Cheng</i> </center></h2>
-	</p>
-</div>
+
 </body>
 </html>
