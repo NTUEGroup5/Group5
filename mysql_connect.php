247f25548c4471a199ffa6de7ef1f31077bdf178
cvindy <cindy545cindy545@gmail.com>
2014-01-10 01:29:59 +0800

    Update mysql_connect.php

--- mysql_connect.php
+++ mysql_connect.php
@@ -5,5 +5,5 @@ $password="gi2";
 $link=mysql_connect($host,$user,$password);
 $query = "SET NAMES 'utf8'";
 $result = mysql_query($query);
-mysql_select_db("Group_5",$link) || die("db error");
+mysql_select_db("group5",$link) || die("db error");
 ?>
