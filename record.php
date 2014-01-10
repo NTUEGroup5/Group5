156ae5ade744b6c6a0257b2163b5e0d6f37dc00a
gaoshix <cyberue73@yahoo.com>
2014-01-10 12:17:45 +0800

    Update record.php

--- record.php
+++ record.php
@@ -9,7 +9,7 @@
 <body>
 
 <body background="http://www.checkfun.com.tw/shop/Stores_APP/images/images_115/CAT%E6%8A%93%E6%AF%9B%E7%B7%9A%E5%9C%96.jpg">
-
+//背景圖BY 高詩翔
 <div id="HEADER">
 	<h2><FONT FACE="微軟正黑體">毛線編織家</FONT></h2>
 </div>
